074f0160b55a0fbfeabc63b249cb45b2bd3bbe62
louderthanme <rubenaguirrelizcano@gmail.com>
2024-06-21 22:06:13 -0600

    aaa

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -3,5 +3,5 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json({ message: "API is working!" });
+  return NextResponse.json({ message: "API is working! finally" });
 }
