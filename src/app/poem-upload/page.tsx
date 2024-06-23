39f861fb70df40eb957cea45a170b1f68d1be6c3
louderthanme <rubenaguirrelizcano@gmail.com>
2024-06-23 14:03:33 -0600

    renaming upload folder to poemupload renaming index to page

---
+++ src/app/poem-upload/page.tsx
@@ -0,0 +1,15 @@
+import { Sheet } from "@mui/joy";
+import TextEditor from "../_components/TextEditor/TextEditor";
+import { EditorProvider } from "../_contexts/Editor.context";
+
+function Upload() {
+  return (
+    <EditorProvider>
+      <Sheet color="danger" variant="solid">
+        <TextEditor />
+      </Sheet>
+    </EditorProvider>
+  );
+}
+
+export default Upload;
