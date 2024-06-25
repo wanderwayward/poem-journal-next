0087b61f856c4e03e87a84e8c1da231a4ebeb8cf
louderthanme <rubenaguirrelizcano@gmail.com>
2024-06-24 20:16:11 -0600

    Made sure User export is default

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,1 +1,25 @@
-import { useUser } from "../_contexts/User.context";
+// src/app/page.tsx
+import { Box, Typography } from "@mui/joy";
+import { FC } from "react";
+
+const User: FC = () => {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "left",
+        marginTop: "60px", // Adjust margin for navbar height
+      }}
+    >
+      <Typography level="title-lg" marginBottom="16px">
+        hi
+      </Typography>
+    </Box>
+  );
+};
+
+export default User;
