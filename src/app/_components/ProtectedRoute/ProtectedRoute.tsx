b921f440ee3bc8e9d3151aec47b4bd3f03204940
wanderwayward <rubenaguirrelizcano@gmail.com>
2024-07-30 23:11:06 -0600

    Trying to make sure cookie is properly saved.

--- src/app/_components/ProtectedRoute/ProtectedRoute.tsx
+++ src/app/_components/ProtectedRoute/ProtectedRoute.tsx
@@ -22,7 +22,11 @@ const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({
     if (!session) {
       // Store the current path the user was trying to access in a cookie
       const callbackUrl = window.location.pathname;
-      Cookies.set("callbackUrl", callbackUrl, { path: "/" });
+      Cookies.set("callbackUrl", window.location.pathname, {
+        path: "/",
+        secure: true, // Ensure cookie is sent over HTTPS
+        sameSite: "None", // Required for cross-site cookie access
+      });
       console.log("Set callbackUrl cookie:", callbackUrl); // Add logging here
       router.push("/auth"); // Redirect to login page
     }
