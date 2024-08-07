620aebce93218c22fc897cf531f1a460cfbd3f98
wanderwayward <rubenaguirrelizcano@gmail.com>
2024-08-06 22:24:42 -0600

    Added extended breakpoints to theme.ts

--- src/app/_theme/theme.ts
+++ src/app/_theme/theme.ts
@@ -110,5 +110,16 @@ export const getDesignTokens = (mode: "light" | "dark") => ({
           },
           divider: "rgba(233, 236, 245, 0.12)", // Light Gray with transparency
         }),
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 600,
+        md: 900,
+        lg: 1200,
+        xl: 1536,
+        xxl: 1920,
+        xxxl: 2560,
+      },
+    },
   },
 });
