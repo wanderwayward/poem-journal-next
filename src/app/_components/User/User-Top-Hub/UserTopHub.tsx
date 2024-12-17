569e329b7545c992a5b07021ef9dc3ac0aee5262
wanderwayward <rubenaguirrelizcano@gmail.com>
2024-12-16 19:35:23 -0600

    Missed an import.

--- src/app/_components/User/User-Top-Hub/UserTopHub.tsx
+++ src/app/_components/User/User-Top-Hub/UserTopHub.tsx
@@ -1,6 +1,6 @@
 import { Grid2 as Grid, Avatar, Typography } from "@mui/material";
 import { FC } from "react";
-import { UserType } from "@/app/_types/Types";
+import { UserType } from "@/editor/types/editorTypes"; //user types need to be moved out of the editor folder but that will be later
 
 interface UserTopHubProps {
 	user: UserType | null;
