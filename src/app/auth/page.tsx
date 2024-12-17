4b6ef52b85cdbe2618b089006528de252f73aee8
wanderwayward <rubenaguirrelizcano@gmail.com>
2024-12-17 14:31:29 -0600

    Starting process of simplifying auth to just a single component rather than sign in and sign up components. Since we are using the google provider for authentication, sign in and sign up are the same thing.

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { Box } from "@mui/material";
 import Auth from "@/features/auth/Auth";
+import SignIn from "@/features/auth/components/SignIn/SignIn";
 
 const AuthPage = () => {
 	return (
 		<Box display="flex" flexDirection="column" alignItems="center">
 			<Auth />
+			<SignIn />
 		</Box>
 	);
 };
