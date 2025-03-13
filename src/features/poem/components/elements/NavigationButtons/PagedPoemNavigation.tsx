2a6dc825659618c7ad4945ba35cc0da58b6de88e
wanderwayward <rubenaguirrelizcano@gmail.com>
2025-03-12 21:28:22 -0600

    Disabled navigation buttons, left and right, when the poem is at the beginning or end of the page.

--- src/features/poem/components/elements/NavigationButtons/PagedPoemNavigation.tsx
+++ src/features/poem/components/elements/NavigationButtons/PagedPoemNavigation.tsx
@@ -1,15 +1,19 @@
 import { NavigateBefore, NavigateNext } from "@mui/icons-material";
 import { Box, IconButton } from "@mui/material";
-import { MouseEventHandler } from "react";
+import { PoemStanzaType } from "@/features/poem/poemTypes";
 
 interface PagedPoemNavigationProps {
 	children: React.ReactNode;
 	handlePageChange: (direction: "left" | "right") => void;
+	currentPage: number;
+	pages: PoemStanzaType[][];
 }
 
 const PagedPoemNavigation: React.FC<PagedPoemNavigationProps> = ({
 	children,
 	handlePageChange,
+	currentPage,
+	pages,
 }) => {
 	return (
 		<Box
@@ -23,6 +27,7 @@ const PagedPoemNavigation: React.FC<PagedPoemNavigationProps> = ({
 				sx={{ fontSize: "2rem" }}
 				onClick={() => handlePageChange("left")}
 				aria-label="previous page"
+				disabled={currentPage === 0}
 			>
 				<NavigateBefore />
 			</IconButton>
@@ -33,6 +38,7 @@ const PagedPoemNavigation: React.FC<PagedPoemNavigationProps> = ({
 				sx={{ fontSize: "2rem" }}
 				onClick={() => handlePageChange("right")}
 				aria-label="next page"
+				disabled={currentPage === pages.length - 2}
 			>
 				<NavigateNext />
 			</IconButton>
